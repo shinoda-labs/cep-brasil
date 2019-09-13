72680338447f2d84b17e15c67aaf66116c065b64
Rodrigo <rodrigo.viegas@ulbra.inf.br>
2019-09-13 00:12:18 -0300

    Botão Consultar

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -7,22 +7,21 @@ class Home extends StatefulWidget {
   _HomeState createState() => _HomeState();
 }
 
-class _HomeState extends State<Home>  with SingleTickerProviderStateMixin{
-
+class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
   TabController _tabController;
+  TextEditingController _controller = TextEditingController();
+  GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
 
   @override
   void initState() {
-    _tabController = new TabController(
-      length: 2,
-      vsync: this
-    );
+    _tabController = new TabController(length: 2, vsync: this);
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: _scaffoldKey,
       appBar: AppBar(
         title: Text('CEP Brasil'),
         backgroundColor: Colors.green,
@@ -43,10 +42,52 @@ class _HomeState extends State<Home>  with SingleTickerProviderStateMixin{
           ],
         ),
       ),
+      floatingActionButton: FloatingActionButton.extended(
+        backgroundColor: Colors.green,
+        icon: Icon(
+          Icons.search,
+          color: Colors.white,
+        ),
+        isExtended: true,
+        label: Text(
+          'Consultar',
+          style: TextStyle(color: Colors.white),
+        ),
+        onPressed: () {
+          if (_controller.text.length < 8) {
+            _scaffoldKey.currentState.showSnackBar(SnackBar(
+              content: Text('CEP inválido'),
+              duration: Duration(seconds: 3),
+            ));
+          }
+        },
+      ),
       body: TabBarView(
         controller: _tabController,
         children: <Widget>[
-          Consultar(),
+          Container(
+            color: Colors.white,
+            child: Padding(
+                padding: EdgeInsets.symmetric(vertical: 16.0, horizontal: 16.0),
+                child: TextField(
+                  cursorColor: Colors.green,
+                  controller: _controller,
+                  keyboardType: TextInputType.number,
+                  textInputAction: TextInputAction.search,
+                  autocorrect: false,
+                  maxLength: 8,
+                  decoration: InputDecoration(
+                      hintText: 'CPF',
+                      icon: Icon(
+                        Icons.room,
+                        color: Colors.green,
+                      ),
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.only(
+                              topLeft: Radius.circular(10.0),
+                              topRight: Radius.circular(10.0)))),
+                )),
+          ),
           Historico()
         ],
       ),
